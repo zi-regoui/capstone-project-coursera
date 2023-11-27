b1e605c5d7f3f60497e79228e1d33b3e9a195601
zi-regoui <reg.zineb99@gmail.com>
2023-11-27 16:31:15 +0100

    second commit

---
+++ littlelemon/restaurant/tests/tests_menu.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from models import MenuItem
+
+class MenuItemTest(TestCase):
+    def test_get_item(self):
+     item = MenuItem.objects.create(title="IceCream", price=80, inventory=100)
+     self.assertEqual(item, "IceCream : 80")
